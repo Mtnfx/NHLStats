2259c7b5c367e2a01ed00cac4a7c7ca85c235dd5
Mtnfx <77549405+Mtnfx@users.noreply.github.com>
2025-06-01 15:40:19 -0400

    Add files via upload
    Update for DataClean.R; converts percentage values (which were originally strings) into numerical values and adds output of cleaned data.

--- DataClean.R
+++ DataClean.R
@@ -3,16 +3,27 @@ library(dplyr) #Used for dataframe manipulation
 library(stringr) #Used for string manipulation
 
 files = list.files("Raw Data/") #Generate list of Excel file names
-i = 0
+i = 0 #This is a counter that tells us how many sheets we have already read.
 for (file in files){ #Loop through all files and merge into single formatted dataframe
-  tmp = read_excel(paste("Raw Data/", file, sep = ""),"QuantHockey", range="A2:AA84")
-  tmp = tmp %>% mutate_at(vars(Rk),funs(str_sub(file,end = -6)))
-  tmp <- rename(tmp,Team = Rk)
+  tmp = read_excel(paste("Raw Data/", file, sep = ""),"QuantHockey", range="A2:AA84") #We want to read the second sheet (the one with data on it) and set the reading range to ignore the top headings.
+  tmp = tmp %>% mutate_at(vars(Rk),funs(str_sub(file,end = -6))) #We don't need the game number, replace this with the team the data corresponds to
+  tmp <- rename(tmp,Team = Rk) #Relabel column to reflect name data
   if (i == 0){
-    final = tmp2
+    final = tmp2 #Initialize final data for data of first spreadsheet
   }
   else{
-    final = bind_rows(final, tmp2)
+    final = bind_rows(final, tmp2) #For all subsequent spreadsheets, append formatted data to final dataframe
   }
   i = i + 1
 }
+
+#Hyphens throw several errors when we try to use various dplyr functions. We replace any hyphens
+for (name in names(final)){
+  names(final)[names(final) == name] = str_replace_all(name,"-","_")
+  name = str_replace_all(name,"-","_")
+  names(final)[names(final) == name] = str_replace_all(name,"%","_PCT")
+}
+
+final =  mutate_at(final,vars(PDO,PDO_A,SH_PCT,SH_PCT_A,FO_PCT,SV_PCT,SV_PCT_A), funs(as.numeric(str_sub(.,end=-2))))
+
+write.table(final, "clean_data.csv", row.names = FALSE, col.names = FALSE)
