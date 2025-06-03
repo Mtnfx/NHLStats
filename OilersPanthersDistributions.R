03840274fc1eb9beb8895041dc776f8fa2b1fd30
Mtnfx <77549405+Mtnfx@users.noreply.github.com>
2025-06-03 18:48:48 -0400

    Updated OilersPanthersDistributions.R (0.2.0)
    Changed sampling method for statistics to 20000 samples, 10000 statistics sets taken from each team.
    
    Sum of density magnitudes did not add to 1 (as I expected), which skewed most distributions to the left. This has been fixed and final distributions are now correct.

--- OilersPanthersDistributions.R
+++ OilersPanthersDistributions.R
@@ -1,7 +1,7 @@
 #The function below will generate a random observation from dens, a given density object.
 #We will need to use this to generate values form all statistics distributions.
 rdens <- function(dens){
-  q = runif(1,0,1)
+  q = runif(1,0,sum(dens$y))
   c_prob = 0
   i = 1
   while(c_prob < q){
@@ -17,87 +17,87 @@ oilers_data = filter(data, Team == "Edmonton Oilers")
 panthers_data = filter(data, Team  == "Florida Panthers")
 
 #We now build density distributions for the Edmonton Oilers:
-o_pdo = oilers_data$PDO %>% density()
-o_pdoa = oilers_data$PDO_A %>% density()
-o_sf = oilers_data$SF %>% density()
-o_sa =  oilers_data$SA %>% density()
+o_pdo = oilers_data$PDO %>% density(from = min(oilers_data$PDO), to = max(oilers_data$PDO))
+o_pdoa = oilers_data$PDO_A %>% density(from = min(oilers_data$PDO_A), to = max(oilers_data$PDO_A))
+o_sf = oilers_data$SF %>% density(from = min(oilers_data$SF), to = max(oilers_data$SF))
+o_sa =  oilers_data$SA %>% density(from = min(oilers_data$SA), to = max(oilers_data$SA))
 #Shot differential will be calculated from already generated data
-o_shpct = oilers_data$SH_PCT %>% density()
-o_shpcta = oilers_data$SH_PCT_A %>% density()
-o_fow = oilers_data$FOW %>% density()
-o_fol = oilers_data$FOL %>% density()
+o_shpct = oilers_data$SH_PCT %>% density(from = min(oilers_data$SH_PCT), to = max(oilers_data$SH_PCT))
+o_shpcta = oilers_data$SH_PCT_A %>% density(from = min(oilers_data$SH_PCT_A), to = max(oilers_data$SH_PCT_A))
+o_fow = oilers_data$FOW %>% density(from = min(oilers_data$FOW), to = max(oilers_data$FOW))
+o_fol = oilers_data$FOL %>% density(from = min(oilers_data$FOL), to = max(oilers_data$FOL))
 #Faceoff differential will be calculated from already generated data
 #Faceoff percentage will be calculated from already generated data
-o_svpct = oilers_data$SV_PCT %>% density()
-o_svpcta = oilers_data$SV_PCT_A %>% density()
-o_hit = oilers_data$HITS %>% density()
-o_hita = oilers_data$HITS_A %>% density()
+o_svpct = oilers_data$SV_PCT %>% density(from = min(oilers_data$SV_PCT), to = max(oilers_data$SV_PCT))
+o_svpcta = oilers_data$SV_PCT_A %>% density(from = min(oilers_data$SV_PCT_A), to = max(oilers_data$SV_PCT_A))
+o_hit = oilers_data$HITS %>% density(from = min(oilers_data$HITS), to = max(oilers_data$HITS))
+o_hita = oilers_data$HITS_A %>% density(from = min(oilers_data$HITS_A), to = max(oilers_data$HITS_A))
 #Hit differential will be calculated from already generated data
-o_bs = oilers_data$BS %>% density()
-o_bsa = oilers_data$BS_A %>% density()
+o_bs = oilers_data$BS %>% density(from = min(oilers_data$BS), to = max(oilers_data$BS))
+o_bsa = oilers_data$BS_A %>% density(from = min(oilers_data$BS_A), to = max(oilers_data$BS_A))
 #Blocked shot differential will be calculated from already generated data.
 
 #We now build density distributions for the Florida Panthers:
-f_pdo = panthers_data$PDO %>% density()
-f_pdoa = panthers_data$PDO_A %>% density()
-f_sf = panthers_data$SF %>% density()
-f_sa =  panthers_data$SA %>% density()
+f_pdo = panthers_data$PDO %>% density(from = min(panthers_data$PDO), to = max(panthers_data$PDO))
+f_pdoa = panthers_data$PDO_A %>% density(from = min(panthers_data$PDO_A), to = max(panthers_data$PDO_A))
+f_sf = panthers_data$SF %>% density(from = min(panthers_data$SF), to = max(panthers_data$SF))
+f_sa =  panthers_data$SA %>% density(from = min(panthers_data$SA), to = max(panthers_data$SA))
 #Shot differential will be calculated from already generated data
-f_shpct = panthers_data$SH_PCT %>% density()
-f_shpcta = panthers_data$SH_PCT_A %>% density()
-f_fow = panthers_data$FOW %>% density()
-f_fol = panthers_data$FOL %>% density()
+f_shpct = panthers_data$SH_PCT %>% density(from = min(panthers_data$SH_PCT), to = max(panthers_data$SH_PCT))
+f_shpcta = panthers_data$SH_PCT_A %>% density(from = min(panthers_data$SH_PCT_A), to = max(panthers_data$SH_PCT_A))
+f_fow = panthers_data$FOW %>% density(from = min(panthers_data$FOW), to = max(panthers_data$FOW))
+f_fol = panthers_data$FOL %>% density(from = min(panthers_data$FOL), to = max(panthers_data$FOL))
 #Faceoff differential will be calculated from already generated data
 #Faceoff percentage will be calculated from already generated data
-f_svpct = panthers_data$SV_PCT %>% density()
-f_svpcta = panthers_data$SV_PCT_A %>% density()
-f_hit = panthers_data$HITS %>% density()
-f_hita = panthers_data$HITS_A %>% density()
+f_svpct = panthers_data$SV_PCT %>% density(from = min(panthers_data$SV_PCT), to = max(panthers_data$SV_PCT))
+f_svpcta = panthers_data$SV_PCT_A %>% density(from = min(panthers_data$SV_PCT_A), to = max(panthers_data$SV_PCT_A))
+f_hit = panthers_data$HITS %>% density(from = min(panthers_data$HITS), to = max(panthers_data$HITS))
+f_hita = panthers_data$HITS_A %>% density(from = min(panthers_data$HITS_A), to = max(panthers_data$HITS_A))
 #Hit differential will be calculated from already generated data
-f_bs = panthers_data$BS %>% density()
-f_bsa = panthers_data$BS_A %>% density()
+f_bs = panthers_data$BS %>% density(from = min(panthers_data$BS), to = max(panthers_data$BS))
+f_bsa = panthers_data$BS_A %>% density(from = min(panthers_data$BS_A), to = max(panthers_data$BS_A))
 #Blocked shot differential will be calculated from already generated data.
 
-#Generate 10000 random samples of each stat. For stats for the Oilers and Against stats for the Panthers are averaged (and vice versa)
-#We produce our collection of observations from the POV of the Oilers.
+#Generate 10000 random samples of each stat.
+#We produce 10000 observations from the POV of the Oilers and 10000 from the POV of the Panthers
 
 #PDO related stats
-pdo = (replicate(10000, rdens(o_pdo)) + replicate(10000, rdens(f_pdoa)))/2
-pdoa = (replicate(10000, rdens(o_pdoa)) + replicate(10000, rdens(f_pdo)))/2
+pdo = c(replicate(10000, rdens(o_pdo)), replicate(10000, rdens(f_pdoa)))
+pdoa = c(replicate(10000, rdens(o_pdoa)), replicate(10000, rdens(f_pdo)))
 
 #Shot Stats
-sf = (replicate(10000, rdens(o_sf)) + replicate(10000, rdens(f_sa)))/2
-sa = (replicate(10000, rdens(o_sa)) + replicate(10000, rdens(f_sf)))/2
+sf = c(replicate(10000, rdens(o_sf)), replicate(10000, rdens(f_sa)))
+sa = c(replicate(10000, rdens(o_sa)), replicate(10000, rdens(f_sf)))
 sd = sf - sa
 
 #Shot Percentages
-shpct = (replicate(10000, rdens(o_shpct)) + replicate(10000, rdens(f_shpcta)))/2
-shpcta = (replicate(10000, rdens(o_shpcta)) + replicate(10000, rdens(f_shpct)))/2
+shpct = c(replicate(10000, rdens(o_shpct)), replicate(10000, rdens(f_shpcta)))
+shpcta = c(replicate(10000, rdens(o_shpcta)), replicate(10000, rdens(f_shpct)))
 
 #Faceoff Stats
-fow = (replicate(10000, rdens(o_fow)) + replicate(10000, rdens(f_fol)))/2
-fol = (replicate(10000, rdens(o_fol)) + replicate(10000, rdens(f_fow)))/2
+fow = c(replicate(10000, rdens(o_fow)), replicate(10000, rdens(f_fol)))
+fol = c(replicate(10000, rdens(o_fol)), replicate(10000, rdens(f_fow)))
 fod = fow-fol
 fopct = fow/(fow+fol)
 
 #Save Percentages
-svpct = (replicate(10000, rdens(o_svpct)) + replicate(10000, rdens(f_svpcta)))/2
-svpcta = (replicate(10000, rdens(o_svpcta)) + replicate(10000, rdens(f_svpct)))/2
+svpct = c(replicate(10000, rdens(o_svpct)), replicate(10000, rdens(f_svpcta)))
+svpcta = c(replicate(10000, rdens(o_svpcta)), replicate(10000, rdens(f_svpct)))
 
 #Hit Statistics
-hits = (replicate(10000, rdens(o_hit)) + replicate(10000, rdens(f_hita)))/2
-hitsa = (replicate(10000, rdens(o_hita)) + replicate(10000, rdens(f_hit)))/2
+hits = c(replicate(10000, rdens(o_hit)), replicate(10000, rdens(f_hita)))
+hitsa = c(replicate(10000, rdens(o_hita)), replicate(10000, rdens(f_hit)))
 hitd = hits - hitsa
 
 #Blocked Shots
-bs = (replicate(10000, rdens(o_bs)) + replicate(10000, rdens(f_bsa)))/2
-bsa = (replicate(10000, rdens(o_bsa)) + replicate(10000, rdens(f_bs)))/2
+bs = c(replicate(10000, rdens(o_bs)), replicate(10000, rdens(f_bsa)))
+bsa = c(replicate(10000, rdens(o_bsa)), replicate(10000, rdens(f_bs)))
 bsd = bs-bsa
 
 #Location - This is is a prediction for game 1 of the Stanley Cup Finals.
 #The Oilers are at home, so we assign every random stats distribution a location code of 1
 
-loc = replicate(10000, 1)
+loc = replicate(20000, 1)
 
 data_out = data.frame(loc, pdo, pdoa, sf, sa, sd, shpct, shpcta, fow, fol, fod,  fopct, svpct, svpcta, hits, hitsa, hitd, bs, bsa, bsd)
 
